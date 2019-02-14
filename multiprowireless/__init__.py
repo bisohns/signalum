55cdf7e3bcd2b5fb264222310bbb717f0ff35ef5
Deven96 <diretnandomnan@gmail.com>
2019-02-14 13:10:55 +0100

    detecting wifi

--- multiprowireless/__init__.py
+++ multiprowireless/__init__.py
@@ -2,6 +2,6 @@ name = "multiprowireless"
 import sys, os
 # insert all modules here
 sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)))
-from bluetooth import *
-from wifi import *
-from zigbee import *
+from _bluetooth import *
+from _wifi import *
+from _zigbee import *
