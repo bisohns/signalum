101e411164ce75554aeff7f4585be3f1d345b6b7
Deven96 <diretnandomnan@gmail.com>
2019-04-12 00:33:47 +0100

    moved scripts to signalum

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setuptools.setup(
         "Operating System :: POSIX",
     ),
     scripts = [
-            'scripts/signalyze'
+            'signalum/scripts/signalyze'
         ],
     package_data={
         '': ['*.*'],
