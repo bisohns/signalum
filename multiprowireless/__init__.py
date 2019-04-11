420041c208ea831e65257ed50fbe7e2e9d015abd
Deven96 <diretnandomnan@gmail.com>
2019-04-11 18:24:48 +0100

    removed dormant multiprowireless folder

--- multiprowireless/__init__.py
+++
@@ -1,7 +0,0 @@
-name = "multiprowireless"
-import sys, os
-# insert all modules here
-sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)))
-from _bluetooth import *
-from _wifi import *
-from _zigbee import *
