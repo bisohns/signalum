85d765309298cbd39347f370a6a682a78aace4a4
Deven96 <diretnandomnan@gmail.com>
2019-04-12 19:39:40 +0100

    made --show-graph and --show-extra-info exclusive

--- signalum/cli.py
+++ signalum/cli.py
@@ -35,7 +35,7 @@ def custom_usage(name=None):
     """
     return f"""
     {get_logo()}
-    usage: signalyze [-h] [-p PROTOCOL] [-o OUTPUT] [--show-graph] [--show-name]
+    usage: signalyze [-h] [-p PROTOCOL] [-o OUTPUT] ([--show-graph] OR [--show-extra-info]) [--show-name]
     """
 
 def runner():
@@ -45,9 +45,10 @@ def runner():
     parser = argparse.ArgumentParser(description='Signalyze', usage=custom_usage())
     parser.add_argument('-p','--protocol', help='A protocol to analyze (default: all)', default='all')
     parser.add_argument('-o', '--output', help='path to store output csv file', default=False)
-    parser.add_argument('--show-graph', action="store_true", help='Show Realtime graph of nearby devices')
+    graph_or_verbose = parser.add_mutually_exclusive_group()
     parser.add_argument('--show-name', action="store_true", help='Show Device name and mac address')
-    parser.add_argument('--show-extra-info', action="store_true", help='Show extra information like services and device classification')
+    graph_or_verbose.add_argument('--show-graph', action="store_true", help='Show Realtime graph of nearby devices')
+    graph_or_verbose.add_argument('--show-extra-info', action="store_true", help='Show extra information like services and device classification')
 
     args = vars(parser.parse_args())
     main(args)
--- signalum/core/_bluetooth.py
+++ signalum/core/_bluetooth.py
@@ -26,6 +26,7 @@ CATEGORY_VALUES = [0, -10, -30, -50, -70]
 OUT_OF_RANGE = (-300, -200)
 NAME_DICT =  dict()
 EXTRA_INFO_DICT = dict()
+CLASS_DICT = dict()
 
 def printpacket(pkt):
     for c in pkt:
@@ -42,16 +43,28 @@ def get_device_extra(addr):
         services = _info_dict.get("services", "XXXX")
         return [major_device, minor_device, services]
     except:
-        print(f"Device MAC {addr} does not exist in details dict")
+        EXTRA_INFO_DICT[addr] = {
+                    "major_device": "",
+                    "minor_device": "",
+                    "services": "",
+                }
+        return ["XXXX", "XXXX", "XXXX"]
 
 def populate_info_dict():
     """
     call to populate the global info dictionary
     """
-    
-    devices = bluetooth.discover_devices(lookup_class=True)
+
     # extract hex value dictionary
-    hex_dict = {i[0]: "%X" %i[1] for i in devices}
+    hex_dict = dict()
+    for i in CLASS_DICT:
+        hex_dict[i] = "%X" %CLASS_DICT[i]
+    # check against odd length hex values
+    for i in hex_dict:
+        if len(hex_dict[i]) % 2 != 0:
+            hex_dict[i] = f"0{hex_dict[i]}"
+
+    # initialize entries in EXTRA_INFO_DICT using vars
     for i in hex_dict:
         if i not in EXTRA_INFO_DICT:
             EXTRA_INFO_DICT[i] = {
@@ -64,7 +77,7 @@ def populate_info_dict():
     # extract bit dictionary using big byte-decode
     bit_dict = {i: bin(int.from_bytes(byte_dict[i], 'big')) for i in byte_dict}
 
-
+    # service bits 
     serv_numbers = [13, 16, 17, 18, 19, 20, 21, 22, 23]
     services = ""
     for i in bit_dict:
@@ -99,9 +112,14 @@ def populate_info_dict():
         if services != "XXXX":
             # run from 13 to 23 excluding (14, 15)
             for x in serv_numbers:
-                # if bit at position -x is 1, append service
-                if bool(int(bit_stream[-x])):
-                    serv += f"{SERVICES[str(x)]}|"
+                # appending 0 before odd-numbered hex values sometimes causes
+                # the bit_stream[-x] query to hit the 'b' flag of the bit stream
+                try:
+                    # if bit at position -x is 1, append service
+                    if bool(int(bit_stream[-x])):
+                        serv += f"{SERVICES[str(x)]}|"
+                except:
+                    pass
             services = serv
 
         EXTRA_INFO_DICT[i] = {
@@ -201,16 +219,21 @@ def device_inquiry_with_with_rssi(sock, show_name=False, show_extra_info=False):
     cmd_pkt = struct.pack("BBBBB", 0x33, 0x8b, 0x9e, duration, max_responses)
     # TODO Optimize code for performance
     # update the global device name dictionary before sending hci cmd(which changes mode)
-    results = []
     headers =["Name", "MAC Address", "RSSI"]
     data = []
-    if show_extra_info:
-        headers.extend(["Major Dev Class", "Minor Dev Class", "Services"])
-        populate_info_dict()
-    if show_name:
-        print("polling for device names...")
-        NAME_DICT.update(dict(bluetooth.discover_devices(lookup_names=True)))
+    results = []
+    if show_extra_info or show_name:
+        devices = bluetooth.discover_devices(lookup_names=True, lookup_class=True)
+        if show_name:
+            update_dict = {i[0]: i[1] for i in devices}
+            NAME_DICT.update(update_dict)
+        if show_extra_info:
+            update_dict = {i[0]: i[2] for i in devices}
+            CLASS_DICT.update(update_dict)
+            headers.extend(["Major Dev Class", "Minor Dev Class", "Services"])
+            populate_info_dict()
     bluez.hci_send_cmd(sock, bluez.OGF_LINK_CTL, bluez.OCF_INQUIRY, cmd_pkt)
+            
 
 
     done = False
@@ -291,7 +314,7 @@ def animate(i, xs, val_dict, ax, sock, show_name=False, show_extra_info=False):
     # limit both axis to VALUES_PER_FRAME values at a time maximum
     xs = xs[-VALUES_PER_FRAME:]
     for i in val_dict:
-        device_name = NAME_DICT[i]
+        device_name = NAME_DICT.get(i, "XXXX")
         val_dict[i] = val_dict[i][-VALUES_PER_FRAME:]
         # if device has dissapeared, append zeros to make up length
         if len(val_dict[i]) < len(xs):
@@ -358,21 +381,21 @@ def bluelyze(**kwargs):
             print("error while setting inquiry mode")
         logging.debug("result: %d" % result)
 
-
+        
     if show_graph:
         # create general figure object 
         fig = plt.figure("Signalum Combined Graphs")
         # change background style
         plt.style.use('seaborn')
+        results = device_inquiry_with_with_rssi(sock, show_name, show_extra_info) 
         
         ax = fig.add_subplot(1, 1, 1)
         xs = []
-        results = device_inquiry_with_with_rssi(sock, show_name=show_name, show_extra_info=show_extra_info) 
         # initialize dictionary to store real time values of devices
         val_dict = {key: list() for key,value,name in results}
         ani = animation.FuncAnimation(fig, animate, fargs=(xs, val_dict, ax, sock, show_name, show_extra_info), interval=100)
         plt.show()    
     else:
         while True:
-            device_inquiry_with_with_rssi(sock, show_name=show_name, show_extra_info=show_extra_info)
+            device_inquiry_with_with_rssi(sock, show_name, show_extra_info)
 
